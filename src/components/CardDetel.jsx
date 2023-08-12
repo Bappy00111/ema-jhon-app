11bb142fa0208e79f542d1a26f5df9a3de5b4db0
“[bappy <[bappymodok0011@gamil.com]>
2023-08-12 03:05:38 -0700

    added cart component calculation

--- src/components/CardDetel.jsx
+++ src/components/CardDetel.jsx
@@ -2,15 +2,27 @@ import React from 'react';
 
 const CardDetel = ({cart}) => {
     console.log(cart)
+
+    let totalPrice = 0;
+    let totalShipping = 0;
+    for(const product of cart){
+        totalPrice = totalPrice + product.price;
+        totalShipping = totalShipping + product.shipping;
+        
+    }
+    const  totaltax = totalPrice * 7 / 100;
+    const grandTotal = totalPrice + totalShipping + totaltax;
+    
+    
     return (
         <div className='bg-[#FF99004D] p-10 h-screen sticky top-[0] rounded'>
             <h1 className='text-xl lg:text-2xl mb-[30px] mt-5'>Order Summary</h1>
             <div className='text-xl  '>
                 <p className='mb-5'>Selected Items: {cart.length}</p>
-                <p className='mb-5'>Total Price: $1140</p>
-                <p className='mb-5'>Total Shipping Charge: $5</p>
-                <p className='mb-5'>Tax: $114</p>
-                <p className='mb-5 font-bold'>Grand Total: $1559</p>
+                <p className='mb-5'>Total Price: ${totalPrice}</p>
+                <p className='mb-5'>Shipping Charge: ${totalShipping}</p>
+                <p className='mb-5'>Tax: ${totaltax.toFixed(2)}</p>
+                <p className='mb-5 font-bold'>Grand Total: ${grandTotal.toFixed(2)}</p>
             </div>
         </div>
     );
